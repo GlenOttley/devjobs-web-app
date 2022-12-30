f33d67a412a7ea866699801771ab20c96b97278d
Glen Ottley <glenottley@gmail.com>
2022-12-30 10:03:34 -0500

    fixed background image issue by prepending forward slash to file path

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header
-	class="min-h-[136px] bg-cover tablet:bg-auto tablet:min-h-[160px] desktop:bg-cover desktop:bg-bottom bg-po bg-no-repeat bg-[url('mobile/bg-pattern-header.svg')] mobile:bg-[url('tablet/bg-pattern-header.svg')] tablet:bg-[url('desktop/bg-pattern-header.svg')]"
+	class="min-h-[136px] bg-cover tablet:bg-auto tablet:min-h-[160px] desktop:bg-cover desktop:bg-bottom bg-po bg-no-repeat bg-[url('/mobile/bg-pattern-header.svg')] mobile:bg-[url('/tablet/bg-pattern-header.svg')] tablet:bg-[url('/desktop/bg-pattern-header.svg')]"
 >
 	<div class="container pt-8">
 		<a href="/">
